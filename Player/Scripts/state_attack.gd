904092a9d5c96c3f02e9d375ef9758dec1935c1e
Th0ngpv <thongpv160605@email.com>
2025-06-26 15:01:52 +0700

    Add attack sound and player deceleration when attack

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -3,18 +3,31 @@ class_name State_Attack extends State
 # initialize var
 var attacking : bool = false
 
+# export var
+@export var attack_sound : AudioStream
+@export_range(1, 20, 0.5) var deceleration : float
+
 # nodes references
 @onready var walk: State_Walk = $"../Walk"
 @onready var idle: State_Idle = $"../Idle"
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 @onready var attack_animation_player: AnimationPlayer = $"../../Sprite2D/AttackEffectSprite/AnimationPlayer"
+@onready var audio: AudioStreamPlayer2D = $"../../Node2D/AudioStreamPlayer2D"
 
 # when the player enter this state
 func Enter() -> void:
 	attacking = true
+#	update animation
 	player.UpdateAnimation("attack")
 	attack_animation_player.play("attack_" + player.animDirection() )
+	
+#	end state when animaiton end
 	animation_player.animation_finished.connect(EndAttack)
+	
+	# add audio 
+	audio.stream = attack_sound
+	audio.pitch_scale = randf_range(0.9, 1.1)
+	audio.play()
 
 # when the player exit this state
 func Exit() -> void:
@@ -24,7 +37,7 @@ func Exit() -> void:
 	
 # what happen durring the state
 func Process(_delta : float) -> State:	
-	player.velocity = Vector2.ZERO
+	player.velocity -= player.velocity * deceleration * _delta
 	
 	if attacking == false:
 		if player.direction == Vector2.ZERO:
